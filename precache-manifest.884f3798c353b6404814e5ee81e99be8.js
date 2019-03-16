ecff914d84c4fb576eac1367d5ceea02dbf4edc4
m.piccinelli <mario.piccinelli@gmail.com>
2019-03-16 17:57:07 +0100

    Updates

---
+++ precache-manifest.884f3798c353b6404814e5ee81e99be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ba6b5d3b26ec766e8be",
+    "url": "/space-enthusiast/static/js/runtime~main.0af08ccf.js"
+  },
+  {
+    "revision": "61439ee550f88128c86a",
+    "url": "/space-enthusiast/static/js/main.a5bab60f.chunk.js"
+  },
+  {
+    "revision": "cef097e6d70645093bcb",
+    "url": "/space-enthusiast/static/js/2.376c4029.chunk.js"
+  },
+  {
+    "revision": "61439ee550f88128c86a",
+    "url": "/space-enthusiast/static/css/main.97f4a988.chunk.css"
+  },
+  {
+    "revision": "739bb41a51169b45b51a21f50dbb6fff",
+    "url": "/space-enthusiast/index.html"
+  }
+];
